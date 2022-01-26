32bcd3e43df796667724da6398ef68e37eb947a5
Ginny Lawrence <vl322@cam.ac.uk>
2022-01-26 10:16:08 +0000

    Complete Task 1B

---
+++ Task1B.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 25 13:20:39 2022
+
+@author: ginnylawrence
+"""
+from floodsystem.utils import sorted_by_key
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_by_distance
+
+
+def run():
+    """Requirements for Task 1B"""
+
+    station_list = build_station_list()
+    distancelist = stations_by_distance(station_list, (52.2053, 0.1218))
+    distancelist = sorted_by_key(distancelist, 2)
+    
+    print("10 closest stations:") 
+    print(distancelist[:10])
+    print("10 furthest stations:")
+    print(distancelist[-10:])
+
+if __name__ == "__main__":
+    print("*** Task 1B: CUED Part IA Flood Warning System ***")
+    run()
